8c3c74fa67b47a57ab098832ac2eedd5d18a6ad8
agick <andreasgoericke@gmail.com>
2019-09-25 17:33:21 +0200

    Update to interface and login functionality added

---
+++ src/app/login-button/login-button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-button',
+  template: `
+  <a nbButton href="https://auth.dtu.dk/dtu/?service=https://se2-webapp04.compute.dtu.dk/" >Login</a>
+  `,
+  styles: []
+})
+export class LoginButtonComponent {
+
+ 
+
+}
