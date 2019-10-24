8c67f109d176d8920ee1900e2705c653ab7dbe29
agick <andreasgoericke@gmail.com>
2019-10-24 10:12:00 +0200

    small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { LoginButtonComponent } from './login-button/login-button.component';
 import { RouterModule, Routes, RouterState } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbButtonModule, NbListModule, NbCardModule, NbDialogService, NbDialogModule, NbDialogRef, NbToastrModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbListModule, NbCardModule, NbDialogService, NbDialogModule, NbDialogRef, NbToastrModule, NbListComponent, NbUserComponent, NbAccordionComponent } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
@@ -22,6 +22,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { FeedbackSmileyRowComponent } from './feedback-smiley-row/feedback-smiley-row.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CreateSurveyComponent } from './create-survey/create-survey.component';
+import { CalendarviewComponent } from './calendarview/calendarview.component';
+
 
 const appRoutes: Routes = [
   { path: 'https://auth.dtu.dk/dtu/?service=se2-webapp04.compute.dtu.dk', component: LoginButtonComponent },
@@ -37,6 +39,7 @@ const appRoutes: Routes = [
     FeedbackComponent,
     FeedbackSmileyRowComponent,
     CreateSurveyComponent,
+    CalendarviewComponent,
   ],
   entryComponents: [
     SurveyComponent,
@@ -58,6 +61,7 @@ const appRoutes: Routes = [
     NbLayoutModule,
     NbEvaIconsModule,
     NbButtonModule,
+    NbAccordionComponent,
     NbCardModule,
     NbListModule,
     NbDialogModule.forRoot(),
