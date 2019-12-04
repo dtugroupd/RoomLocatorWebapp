04afb269f6d6c795cfd5783d0db7aa07f994b63e
Anders Wiberg Olsen <awo@it-minds.dk>
2019-12-04 23:22:57 +0100

    Sending simple messages

---
+++ src/app/_services/signal-rservice.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRServiceService {
+  public message: string;
+  public connection: HubConnection;
+
+  constructor() { }
+
+  public start(token: string) {
+    this.connection = new HubConnectionBuilder()
+      .configureLogging(LogLevel.Information)
+      .withUrl(`https://localhost:5001/api/connect`, { accessTokenFactory: () => token })
+      .withAutomaticReconnect()
+      .build();
+      this.connection.start().then(() => {
+        console.info("Connected using SignalR");
+      }).catch(err => console.error(`SignalR Connection failed with ${err}`));
+
+      this.connection.on('message', x => console.log(`Received Message:`, x));
+  }
+}
