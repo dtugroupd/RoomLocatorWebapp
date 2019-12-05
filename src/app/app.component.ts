fed3b0f298ec6ecacc6c2656f6382f1eca4d4769
Hadi Horani <hadihorani@hotmail.com>
2019-12-05 02:22:45 +0100

    if 401 is intercepted, the user will be logged out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -141,8 +141,6 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     });
 
-    this.store.dispatch(new GetSurveys());
-
     this.router.events.subscribe(x => {
       if (x instanceof NavigationEnd) {
         switch (x.urlAfterRedirects) {
